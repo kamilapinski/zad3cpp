3a9b691ea5898b75196cbcab85e81f0199269bed
Kamil Łapiński <kamill2004@gmail.com>
2024-11-05 16:13:53 +0100

    Dodanie konstruktorów Tournament

--- knights.cpp
+++ knights.cpp
@@ -11,16 +11,46 @@ class Knight {
 constexpr Knight TRAINEE_KNIGHT; // TODO
 
 class Tournament {
-        std::queue<Knight> tournament_list;
+    private:
+        std::queue<Knight> fight_list;
         std::stack<Knight> lost_list;
 
     public:
         Tournament(std::initializer_list<Knight> s) {
             for (auto knight : s) {
-                tournament_list.push(knight);
+                fight_list.push(knight);
             }
 
-            if (s.size() == 0) tournament_list.push(TRAINEE_KNIGHT);
+            if (s.size() == 0) fight_list.push(TRAINEE_KNIGHT);
         }
+
+        Tournament(const Tournament& that) : fight_list(that.fight_list), lost_list(that.lost_list) {
+            // done
+        }
+
+        Tournament(Tournament&& that) :
+            fight_list(std::move(that.fight_list)),
+            lost_list(std::move(that.lost_list))
+        {
+            // done
+        }
+
+        Tournament& operator=(const Tournament& that) {
+            if (this != &that) {
+                fight_list = that.fight_list;
+                lost_list = that.lost_list;
+            }
+            return *this;
+        }
+
+        Tournament& operator=(Tournament&& that) {
+            if (this != &that) {
+                fight_list = std::move(that.fight_list);
+                lost_list = std::move(that.lost_list);
+            }
+            return *this;
+        }
+
+
 
 };
--- knights.h
+++ knights.h
@@ -8,6 +8,10 @@ class Knight {
 class Tournament {
     public:
         Tournament(std::initializer_list<Knight> s);
+        Tournament(const Tournament& that);
+        Tournament(Tournament&& that);
+        Tournament& operator=(const Tournament& that);
+        Tournament& operator=(Tournament&& that);
 };
 
 #endif KNIGHT_H
