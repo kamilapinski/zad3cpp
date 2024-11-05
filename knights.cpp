2dae021490a9cac62caf8d81ee98028d7e774a08
Kamil Łapiński <kamill2004@gmail.com>
2024-11-05 15:42:22 +0100

    Podstawy2

--- knights.cpp
+++ knights.cpp
@@ -1,1 +1,15 @@
 #include "knights.h"
+
+#include <iostream>
+#include <queue>
+#include <stack>
+
+class Knight {
+
+};
+
+constexpr Knight TRAINEE_KNIGHT; // TODO
+
+class Tournament {
+        
+};
--- knights.h
+++ knights.h
@@ -1,6 +1,12 @@
 #ifndef KNIGHTS_H
 #define KNIGHTS_H
 
+class Knight {
 
+};
+
+class Tournament {
+    
+};
 
 #endif KNIGHT_H
