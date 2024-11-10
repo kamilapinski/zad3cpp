bb6fea797c6d416992f2c27e3d00ac9386e2127c
Arczi <108293311+ArcZeus@users.noreply.github.com>
2024-11-10 19:49:30 +0100

    Implementacja klasy Knight

--- knights.cpp
+++ knights.cpp
@@ -1,13 +1,103 @@
 #include "knights.h"
+#include <compare>
 
-#include <iostream>
-#include <list>
+constexpr size_t Knight::get_gold() {
+    
+    return gold;
+}
 
-class Knight {
+constexpr size_t Knight::get_armour_class() {
+    
+    return armour_class;
+}
 
-};
+constexpr size_t Knight::get_weapon_class() {
 
-constexpr Knight TRAINEE_KNIGHT; // TODO
+    return weapon_class;
+}
+
+constexpr size_t Knight::give_gold() {
+    size_t tmp_gold = gold;
+    gold = 0;
+    return tmp_gold;
+}
+
+constexpr void Knight::take_gold(size_t amount) {
+    gold += amount;
+}
+
+constexpr void Knight::change_weapon(size_t new_weapon_class) {
+    weapon_class = new_weapon_class;
+}
+
+constexpr size_t Knight::give_up_weapon() {
+    size_t temp_weapon = weapon_class;
+    weapon_class = 0;
+    return weapon_class;
+}
+
+constexpr void Knight::change_armour(size_t new_armour_class) {
+    armour_class = new_armour_class;
+}
+
+constexpr size_t Knight::take_off_armour() {
+    size_t temp_armour = armour_class;
+    armour_class = 0;
+    return temp_armour;
+}
+
+constexpr Knight & Knight::operator+=(Knight &rhs) {
+    take_gold(rhs.give_gold()); // Chyba tego oczekiwali.
+
+    if (rhs.weapon_class > this->weapon_class) {
+        change_weapon(rhs.give_up_weapon());
+    
+    }
+    if (rhs.armour_class > this->armour_class) {
+        change_armour(rhs.take_off_armour());
+    }
+
+    return *this;
+} // DONE
+
+constexpr Knight Knight::operator+(const Knight& k) {
+    size_t total_gold = std::min(gold + k.gold, MAX_GOLD);
+    size_t best_armor_class = std::max(armour_class, k.armour_class);
+    size_t best_weapon_class = std::max(weapon_class, k.weapon_class);
+
+    return Knight(total_gold, best_armor_class, best_weapon_class);
+} // DONE
+
+constexpr bool Knight::operator==(const Knight& other) {
+
+    return (*this <=> other) == std::strong_ordering::equal;
+} // DONE
+
+constexpr std::strong_ordering Knight::operator<=>(const Knight& other) { // Nie wiem czy da się to zrobić bardziej elegancko.
+    bool thisWins = (this->weapon_class > other.armour_class && this->armour_class >= other.weapon_class);
+    bool otherWins = (other.weapon_class > this->armour_class && other.armour_class >= this->weapon_class);
+
+    if (thisWins && !otherWins) {
+        
+        return std::strong_ordering::greater;
+    }
+    if (otherWins && !thisWins) {
+
+        return std::strong_ordering::less;
+    }
+
+    // Obaj rycerze mają broń silniejszą od zbroi przeciwnika.
+    if (this->weapon_class > other.armour_class && other.weapon_class > this->armour_class) {
+        if (this->armour_class != other.armour_class) {
+        
+            return this->armour_class <=> other.armour_class;
+        }
+
+        return this->weapon_class <=> other.weapon_class;
+    }
+
+    return std::strong_ordering::equal;
+} // DONE
 
 class Tournament {
     private:
--- knights.h
+++ knights.h
@@ -4,8 +4,49 @@
 #include <initializer_list>
 
 class Knight {
+    private:        
+        size_t gold;
+        size_t armour_class;
+        size_t weapon_class;
+        
+    public:
+        Knight() = delete;
+        static constexpr size_t MAX_GOLD = SIZE_MAX;
+        constexpr Knight(size_t g, size_t a, size_t w) :
+            gold(g), 
+            armour_class(a), 
+            weapon_class(w) 
+        { 
+        }
 
+        constexpr Knight(const Knight&) = default;
+        constexpr Knight& operator=(const Knight&) = default;
+        constexpr Knight(Knight&&) = default;
+        constexpr Knight& operator= (Knight&&) = default;
+
+        constexpr size_t get_gold();
+        constexpr size_t get_armour_class();
+        constexpr size_t get_weapon_class();
+        
+        constexpr size_t give_gold();
+        constexpr void take_gold(size_t amount);
+        constexpr void change_weapon(size_t newWeaponClass);
+        constexpr size_t give_up_weapon();
+        constexpr void change_armour(size_t newArmorClass);
+        constexpr size_t take_off_armour();
+
+        constexpr Knight& operator+=(Knight& rhs);
+        constexpr Knight operator+(const Knight& k);
+        constexpr bool operator==(const Knight& other); 
+        constexpr std::strong_ordering operator<=>(const Knight& other);
+        friend std::ostream& operator<<(std::ostream& os, const Knight& knight) {
+            os << "(" << knight.gold << ", " << knight.weapon_class
+               << ", " << knight.armour_class << ")";
+            return os;
+        }
 };
+
+constexpr Knight TRAINEE_KNIGHT(0, 1, 1);
 
 class Tournament {
     public:
