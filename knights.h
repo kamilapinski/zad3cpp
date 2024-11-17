b2c37851aa04432103ad19cc363e98acd712e7a8
Kamil Łapiński <kamill2004@gmail.com>
2024-11-17 14:35:26 +0100

    Przeniosłem wszystko do .h i pododawałem inline

--- knights.h
+++ knights.h
@@ -20,59 +20,59 @@ class Knight {
     public:
         //Knight() = delete;
         static constexpr size_t MAX_GOLD = SIZE_MAX;
-        constexpr Knight(size_t g, size_t w, size_t a) :
+        inline constexpr Knight(size_t g, size_t w, size_t a) :
             gold(g), 
             weapon_class(w),
             armour_class(a) 
         { 
         }
 
-        constexpr Knight(const Knight&) = default;
-        constexpr Knight& operator=(const Knight&) = default;
-        constexpr Knight(Knight&&) = default;
-        constexpr Knight& operator= (Knight&&) = default;
+        inline constexpr Knight(const Knight&) = default;
+        inline constexpr Knight& operator=(const Knight&) = default;
+        inline constexpr Knight(Knight&&) = default;
+        inline constexpr Knight& operator= (Knight&&) = default;
 
-        constexpr size_t get_gold() const {
+        inline constexpr size_t get_gold() const {
             return gold;
         }
-        constexpr size_t get_armour_class() const {
+        inline constexpr size_t get_armour_class() const {
             return armour_class;
         }
-        constexpr size_t get_weapon_class() const {
+        inline constexpr size_t get_weapon_class() const {
             return weapon_class;
         }
         
-        constexpr size_t give_gold() {
+        inline constexpr size_t give_gold() {
             size_t tmp_gold = gold;
             gold = 0;
             return tmp_gold;
         }
 
-        constexpr void take_gold(size_t amount) {
+        inline constexpr void take_gold(size_t amount) {
             gold = (gold > MAX_GOLD - amount) ? MAX_GOLD : gold + amount;
         }
  
-        constexpr void change_weapon(size_t new_weapon_class) {
+        inline constexpr void change_weapon(size_t new_weapon_class) {
             weapon_class = new_weapon_class;
         }
 
-        constexpr size_t give_up_weapon() {
+        inline constexpr size_t give_up_weapon() {
             size_t temp_weapon = weapon_class;
             weapon_class = 0;
             return temp_weapon;
         }
 
-        constexpr void change_armour(size_t new_armour_class) {
+        inline constexpr void change_armour(size_t new_armour_class) {
             armour_class = new_armour_class;
         }
 
-        constexpr size_t take_off_armour() {
+        inline constexpr size_t take_off_armour() {
             size_t temp_armour = armour_class;
             armour_class = 0;
             return temp_armour;
         }
 
-        constexpr Knight& operator+=(Knight& rhs)  {
+        inline constexpr Knight& operator+=(Knight& rhs)  {
             take_gold(rhs.give_gold()); // Chyba tego oczekiwali.
 
             (rhs.weapon_class > this->weapon_class) ? change_weapon(rhs.give_up_weapon()) : change_weapon(this->weapon_class);
@@ -82,7 +82,7 @@ class Knight {
             return *this;
         } // DONE
 
-        constexpr Knight operator+(const Knight& k) const {
+        inline constexpr Knight operator+(const Knight& k) const {
             size_t total_gold = (gold > MAX_GOLD - k.gold) ? MAX_GOLD : gold + k.gold;
             size_t best_weapon_class = std::max(weapon_class, k.weapon_class);
             size_t best_armor_class = std::max(armour_class, k.armour_class);
@@ -112,19 +112,19 @@ class Knight {
             ;
         }
 
-        constexpr bool operator>(const Knight& other) const {
+        inline constexpr bool operator>(const Knight& other) const {
             return (*this <=> other) == std::strong_ordering::greater;
         }
 
-        constexpr bool operator<(const Knight& other) const {
+        inline constexpr bool operator<(const Knight& other) const {
             return (*this <=> other) == std::strong_ordering::less;
         }
 
-        constexpr bool operator==(const Knight& other) const {
+        inline constexpr bool operator==(const Knight& other) const {
             return (*this <=> other) == std::strong_ordering::equal;
         }
 
-        friend std::ostream& operator<<(std::ostream& os, const Knight& knight) {
+        friend inline std::ostream& operator<<(std::ostream& os, const Knight& knight) {
             os << "(" << knight.gold << " gold, " << knight.weapon_class
                << " weapon class, " << knight.armour_class << " armour class)\n";
             return os;
@@ -138,21 +138,103 @@ class Tournament {
         std::list<Knight> fight_list;
         std::list<Knight> lost_list;
 
-        void payoff(Knight& winner, Knight& loser);
-        void remove_fighting_knights(const Knight& knight);
+        inline void payoff(Knight& winner, Knight& loser) {
+            winner += loser; // Knight musi mieć zdefiniowany ten operator
+            fight_list.push_back(winner);
+            lost_list.push_front(loser);
+        }
+
+        inline void remove_fighting_knights(const Knight& knight) {
+            fight_list.remove_if([&knight](const Knight& other) {
+                return (knight.get_gold() == other.get_gold() && knight.get_weapon_class() == other.get_weapon_class() && knight.get_armour_class() == other.get_armour_class()) ? true : false;;
+            });
+        }
 
     public:
-        Tournament() = delete;
-        Tournament(std::initializer_list<Knight> s);
-        Tournament(const Tournament& that);
-        Tournament(Tournament&& that);
-        Tournament& operator=(const Tournament& that);
-        Tournament& operator=(Tournament&& that);
-        Tournament& operator+=(const Knight& knight);
-        Tournament& operator-=(const Knight& knight);
-        const std::list<Knight>::iterator play();
-        const std::list<Knight>::iterator no_winner();
-        size_t size() const;
+        Tournament(std::initializer_list<Knight> s) {
+            fight_list = std::list<Knight>();
+            if (s.size() == 0) {
+                fight_list.push_back(TRAINEE_KNIGHT);
+            } else {
+                for (auto knight : s) {
+                    fight_list.push_back(knight);
+                }
+            }
+        }
+
+        inline Tournament(const Tournament& that) : 
+        fight_list(that.fight_list), 
+        lost_list(that.lost_list) {
+            // done
+        }
+
+        inline Tournament(Tournament&& that) : 
+        fight_list(std::move(that.fight_list)), 
+        lost_list(std::move(that.lost_list)) {
+            // done
+        }
+
+        inline Tournament& operator=(const Tournament& that) {
+            if (this != &that) {
+                fight_list = that.fight_list;
+                lost_list = that.lost_list;
+            }
+            return *this;
+        }
+
+        inline Tournament& operator=(Tournament&& that) {
+            if (this != &that) {
+                fight_list = std::move(that.fight_list);
+                lost_list = std::move(that.lost_list);
+            }
+            return *this;
+        }
+
+        inline Tournament& operator+=(const Knight& knight) {
+            lost_list.clear();
+            fight_list.push_back(knight);
+            return *this;
+        }
+
+        inline Tournament& operator-=(const Knight& knight) {
+            lost_list.clear();
+            this->remove_fighting_knights(knight);
+            return *this;
+        }
+
+        const std::list<Knight>::iterator play() {
+            lost_list.clear();
+
+            while (fight_list.size() > 1) {
+                Knight first = fight_list.front();
+                fight_list.pop_front();
+                Knight second = fight_list.front();
+                fight_list.pop_front();
+
+                if (first > second) // Knight musi mieć zdef. ten operator
+                    payoff(first, second);
+                else if (first < second)
+                    payoff(second, first);
+                else {
+                    // kolejność taka sama jak na fight_list
+                    lost_list.push_front(second);
+                    lost_list.push_front(first);
+                }
+            }
+
+            if (fight_list.empty())
+                return fight_list.end();
+            return fight_list.begin();
+        }
+
+        inline const std::list<Knight>::iterator no_winner() {
+            return fight_list.end();
+        }
+
+        inline size_t size() const {
+            return fight_list.size() + lost_list.size();
+            // chociażby dlatego lost_list nie jest typu forward_list
+        }
 
         friend std::ostream& operator<<(std::ostream& os, const Tournament& tournament) {
             for (const Knight knight : tournament.fight_list) {
